7c735813bf5e2ed618942ba35c30701cb5464a35
HOLO-png <69238651+HOLO-png@users.noreply.github.com>
2022-10-08 10:25:00 +0700

    push-project-client

--- client/sdk/Flex/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Grid, GridProps } from '@mui/material';
-
-export const Flex = (props: GridProps) => {
-  return <Grid {...props} display="flex" container />;
-};
-
-export const FlexItem = (props: GridProps) => {
-  return <Grid {...props} display="flex" item />;
-};
--- client/sdk/FormProvider/constants.ts
+++
@@ -1,9 +0,0 @@
-export enum FormInputEnum {
-  INPUT = 'input',
-  NUMBER = 'number',
-  SELECT = 'select',
-  RADIO = 'radio',
-  CHECKBOX = 'checkbox',
-  PASSWORD = 'password',
-  DATE = 'date'
-}
